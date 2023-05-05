9a90c96c5ad3e59042bc128bba6e1c25e9578286
Steph-Z <steph.zieger@gmail.com>
2023-05-05 20:51:34 +0200

    get the files and branch to my home pc, nothing finished in this commit

--- utils/loader_steve.py
+++ utils/loader_steve.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 #in this file we will collect the functions/utilities to load the preprocessed data for training purposes
 #to do- merge the different preprocessing files and create 1 preprocessing function
 #Stephan and Johannes
@@ -6,26 +8,40 @@
 #work in progress
 
 #define the dictionary to load in the data efficiently
-dtypes={
-    "level_group": "category",
-    'level': np.uint8,
-    'event_name_nunique': np.uint8,
-    'name_nunique':np.uint8,
-    'fqid_nunique':np.uint8,
-    'room_fqid_nunique':np.uint8,
-    'text_fqid_nunique':np.uint8, 
-    'hover_duration_mean': np.float32,
-    'difference_clicks_mean':np.float32,
-    'elapsed_time_std':np.float32,
-    'page_std':np.float32,
-    'room_coor_x_std':np.float32,
-    'room_coor_y_std':np.float32,
-    'screen_coor_x_std':np.float32,
-    'screen_coor_y_std':np.float32,
-    'hover_duration_std':np.float32,
-    'difference_clicks_std':np.float32,
-    'index_sum_of_actions':np.uint16,
-    'difference_clicks_max':np.float32,
-    'elapsed_time_max':np.float32,
-    'clicks_per_second':np.float32
-    }
+
+def load_train_data(file_path: str = "data/processed/train.csv", dtypes: = dict = None, n_rows: int = None)
+    if dtypes is None:
+    
+        dtypes={
+            'level': np.uint8,  
+            'session_id':np.int64,
+            'level_group':'category',
+            'event_name':np.int64,
+            'name':np.int64,
+            'fqid':np.int64,
+            'room_fqid':np.int64,
+            'text_fqid':np.int64,
+            'fullscreen':'category',
+            'hq':'category',
+            'music':'category',
+            'hover_duration_mean':np.float32,
+            'difference_clicks_mean':np.float64,
+            'elapsed_time_std':np.float64,
+            'page_std':np.float64,
+            'room_coor_x_std':np.float64,
+            'room_coor_y_std':np.float64,
+            'screen_coor_x_std':np.float64,
+            'screen_coor_y_std':np.float64,
+            'hover_duration_std':np.float64,
+            'difference_clicks_std':np.float64,
+            'index_sum_of_actions':np.int32,
+            'difference_clicks_max':np.float64,
+            'elapsed_time_max':np.float64,
+            'clicks_per_second':np.float64}
+        
+    if n_rows = None:
+        df = pd.read_csv(file_path, dtype = dtypes)
+    else:
+        df = pd.read_csv(file_path, dtype = dtypes, nrows= n_rows)
+        
+    return df
